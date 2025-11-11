a21a35807c6ea4959321dbe2f05a5c6edb3f3808
shaniaesguerra <shaniae47@gmail.com>
2025-11-11 13:12:05 -0700

    W03 Activity: changed .mjs file name, separated functions into designated files

---
+++ week03/scripts/output.mjs
@@ -0,0 +1,22 @@
+/*
+    This file contains the functions that are use to render
+    the course title and sections to the page.
+*/
+
+function setTitle(course) {
+    document.querySelector("#courseName").textContent = course.name;
+    document.querySelector("#courseCode").textContent = course.code;
+}
+
+function renderSections(sections) {
+    const html = sections.map(
+        (section) => `<tr>
+    <td>${section.sectionNumber}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.instructor}</td></tr>`
+    );
+    document.querySelector("#sections").innerHTML = html.join("");
+}
+
+export function setTitle(course) { }
+export function renderSections(sections){}
---
+++ week03/scripts/sections.mjs
@@ -0,0 +1,16 @@
+/*
+    This file contains the function that populates the 
+    section selection element on the page
+*/
+
+function setSectionSelection() {
+    const sectionSelect = document.querySelector("#sectionNumber");
+    byuiCourse.sections.forEach((section) => {
+        const option = document.createElement("option");
+        option.value = section.sectionNumber;
+        option.textContent = `${section.sectionNumber}`;
+        sectionSelect.appendChild(option);
+    });
+}
+
+export function populateSections(sections) {}
