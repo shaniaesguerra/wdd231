c2b384613c05863bddc3a099e2d94206d8aa283a
shaniaesguerra <shaniae47@gmail.com>
2025-11-14 16:00:50 -0700

    W03 Chamber Home Page: Styling on Spotlight cards, and changes in js file for populating elemetnt

--- chamber/scripts/spotlight-cards.js
+++ chamber/scripts/spotlight-cards.js
@@ -18,10 +18,8 @@ async function getBusinessData() {
 
 //Create content for Grid view
 const displayBusinessGrid = (businesses) => {
-    let spotCount = 0; // keep track of businesses to place in spotlight
-    
     businesses.forEach(business => {
-        if ((business.membership_lvl == 3 || business.membership_lvl == 2) && spotCount <= 3) {
+        if ((business.membership_lvl == 3 || business.membership_lvl == 2)) {
             /************ FOR POPULATING SPOTLIGHT CARDS IN LANDDING PAGE ****************/
             //Create elements to build for each card:
             let card = document.createElement('section');
@@ -31,8 +29,8 @@ const displayBusinessGrid = (businesses) => {
             //info container contents:
             let address = document.createElement('p');
             let phoneNumber = document.createElement('p');
-            let email = document.createElement('p');
             let website = document.createElement("a");
+            let membershipLvl = document.createElement('p');
 
             //Build icon (img) content:
             icon.setAttribute('src', business.image);
@@ -47,7 +45,13 @@ const displayBusinessGrid = (businesses) => {
             //Build the (p) tags with content: 
             address.textContent = `${business.address.street}, ${business.address.city}, ${business.address.country} ${business.address.zip}`;
             phoneNumber.textContent = `${business.phone_number}`;
-            email.textContent = `${business.email}`;
+
+            if (business.membership_lvl == 3) {
+                membershipLvl.textContent = `Membership Level: Gold`;
+            }
+            else if (business.membership_lvl == 2) {
+                membershipLvl.textContent = `Membership Level: Silver`;
+            }
 
             //Build website(a) content:
             website.href = `${business.url}`;
@@ -59,14 +63,13 @@ const displayBusinessGrid = (businesses) => {
             card.appendChild(businessName);
             card.appendChild(address);
             card.appendChild(phoneNumber);
-            card.appendChild(email);
             card.appendChild(website);
+            card.appendChild(membershipLvl);
 
         
             //Append the card(section) to businessCard(#div):
             spotlightCards.appendChild(card);  
         }
-        spotCount = spotCount + 1;
     });
 }
 
