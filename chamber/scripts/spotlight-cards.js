f4489683fb500c2921a79d0c73710d1ac214f4ba
shaniaesguerra <shaniae47@gmail.com>
2025-11-15 13:03:21 -0700

    W03 Chamber Home Page: Randomized Spotlight cards

--- chamber/scripts/spotlight-cards.js
+++ chamber/scripts/spotlight-cards.js
@@ -18,58 +18,79 @@ async function getBusinessData() {
 
 //Create content for Grid view
 const displayBusinessGrid = (businesses) => {
-    businesses.forEach(business => {
-        if ((business.membership_lvl == 3 || business.membership_lvl == 2)) {
-            /************ FOR POPULATING SPOTLIGHT CARDS IN LANDDING PAGE ****************/
-            //Create elements to build for each card:
-            let card = document.createElement('section');
-            let icon = document.createElement('img');
-            let businessName = document.createElement('h2');
+    //Filter businesses to inclue Level 2 and 3 members only:
+    let memberBusinesses = businesses.filter(business => 
+        business.membership_lvl === 3 || business.membership_lvl === 2
+    );
 
-            //info container contents:
-            let address = document.createElement('p');
-            let phoneNumber = document.createElement('p');
-            let website = document.createElement("a");
-            let membershipLvl = document.createElement('p');
+    //Shuffle the filtered list:
+    for (let i = memberBusinesses.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // random number to change index with
 
-            //Build icon (img) content:
-            icon.setAttribute('src', business.image);
-            icon.setAttribute('alt', `Icon for ${business.name}`);
-            icon.setAttribute('loading', 'lazy');
-            icon.setAttribute('width', '300');
-            icon.setAttribute('height', 'auto');
+        //Swap items in the list:
+        let temp = memberBusinesses[i];
+        memberBusinesses[i] = memberBusinesses[j];
+        memberBusinesses[j] = temp;
+    }
 
-            //Build businessName (h2) content
-            businessName.textContent = `${business.name}`;
+    //Store the top 3 businesses in the filtered list
+    const topThreeBusiness = memberBusinesses.slice(0, 3);
 
-            //Build the (p) tags with content: 
-            address.textContent = `${business.address.street}, ${business.address.city}, ${business.address.country} ${business.address.zip}`;
-            phoneNumber.textContent = `${business.phone_number}`;
+    //Display just the first three businesses:
+    topThreeBusiness.forEach(business => {
 
-            if (business.membership_lvl == 3) {
-                membershipLvl.textContent = `Membership Level: Gold`;
-            }
-            else if (business.membership_lvl == 2) {
-                membershipLvl.textContent = `Membership Level: Silver`;
-            }
+        /************ FOR POPULATING SPOTLIGHT CARDS IN LANDDING PAGE ****************/
+        //Create elements to build for each card:
+        let card = document.createElement('section');
+        let icon = document.createElement('img');
+        let businessName = document.createElement('h2');
 
-            //Build website(a) content:
-            website.href = `${business.url}`;
-            website.target = '_blank';
-            website.textContent = business.url;
+        //info container contents:
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let website = document.createElement("a");
+        let membershipLvl = document.createElement('p');
 
-            //Append all content to card(section):
-            card.appendChild(icon);
-            card.appendChild(businessName);
-            card.appendChild(address);
-            card.appendChild(phoneNumber);
-            card.appendChild(website);
-            card.appendChild(membershipLvl);
+        //Build icon (img) content:
+        icon.setAttribute('src', business.image);
+        icon.setAttribute('alt', `Icon for ${business.name}`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '300');
+        icon.setAttribute('height', 'auto');
 
-        
-            //Append the card(section) to businessCard(#div):
-            spotlightCards.appendChild(card);  
+        //Build businessName (h2) content
+        businessName.textContent = `${business.name}`;
+
+        //Build the (p) tags with content: 
+        address.textContent = `${business.address.street}, ${business.address.city}, ${business.address.country} ${business.address.zip}`;
+        phoneNumber.textContent = `${business.phone_number}`;
+
+        if (business.membership_lvl == 3) {
+            membershipLvl.textContent = `👑 Gold Member 👑`;
         }
+        else if (business.membership_lvl == 2) {
+            membershipLvl.textContent = `⚔️ Silver Member ⚔️`;
+        }
+        else {
+            membershipLvl.textContent = `🏅 Member 🏅`;
+        }
+
+        //Build website(a) content:
+        website.href = `${business.url}`;
+        website.target = '_blank';
+        website.textContent = business.url;
+
+        //Append all content to card(section):
+        card.appendChild(icon);
+        card.appendChild(businessName);
+        card.appendChild(address);
+        card.appendChild(phoneNumber);
+        card.appendChild(website);
+        card.appendChild(membershipLvl);
+
+
+        //Append the card(section) to businessCard(#div):
+        spotlightCards.appendChild(card);
     });
 }
 
