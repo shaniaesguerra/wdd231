d92f1e33a76607744529646876a9ad6367c88433
shaniaesguerra <shaniae47@gmail.com>
2025-11-19 14:52:14 -0700

    W04 Activity: adding multiple dialog boxes and event listeners

--- week04/startLab/js/scripts.js
+++ week04/startLab/js/scripts.js
@@ -1,13 +1,26 @@
 const openButton = document.querySelector("#openButton");
 const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
+const dialogBoxText = document.querySelector("#dialogBox div");
 
 //"Show the dialog" button opnes the dialog modally
-openButton.addEventListener("click", () => {
+openButton1.addEventListener("click", () => {
     dialogBox.showModal(); //make modal show (pop-up show)
+    dialogBoxText.innerHTML = `One Apple contains 95 calories`;
+});
+
+openButton2.addEventListener("click", () => {
+    dialogBox.showModal(); //make modal show (pop-up show)
+    dialogBoxText.innerHTML = `One Orange contains 45 calories`;
+});
+
+openButton3.addEventListener("click", () => {
+    dialogBox.showModal(); //make modal show (pop-up show)
+    dialogBoxText.innerHTML = `One Banana contains 105 calories`;
 });
 
 //"Close" button closes the dialog
 closeButton.addEventListener("click", () => {
     dialogBox.close(); //close pop-up
 });
+
